bc0fe5c61696c8408c559c7de2ac7ff8eb0a71cf
elsaho <59989701+elsaho@users.noreply.github.com>
2023-04-05 22:31:37 -0700

    Add files via upload

---
+++ assembly.s
@@ -0,0 +1,99 @@
+	.file	"assembly.s"
+	.text
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"Guess a number between 1 and 5: "
+.LC1:
+	.string	"%d"
+.LC2:
+	.string	"You win!"
+.LC3:
+	.string	"You lose!"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB6:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movl	$0, -28(%rbp)
+	movl	$5, -24(%rbp)
+	movl	$1, -20(%rbp)
+	movl	-24(%rbp), %eax
+	subl	-20(%rbp), %eax
+	addl	$1, %eax
+	movl	%eax, -16(%rbp)
+	movl	$0, %edi
+	call	time@PLT
+	movl	%eax, %edi
+	call	srand@PLT
+	call	rand@PLT
+	cltd
+	idivl	-16(%rbp)
+	movl	-20(%rbp), %eax
+	addl	%edx, %eax
+	movl	%eax, -12(%rbp)
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-32(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	movl	-32(%rbp), %eax
+	cmpl	%eax, -12(%rbp)
+	jne	.L2
+	leaq	.LC2(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	jmp	.L3
+.L2:
+	leaq	.LC3(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+.L3:
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	subq	%fs:40, %rdx
+	je	.L5
+	call	__stack_chk_fail@PLT
+.L5:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 11.3.0-1ubuntu1~22.04) 11.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
