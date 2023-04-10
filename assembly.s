d8e64b0d49522c9f2058055f78309b25b274baac
elsaho <59989701+elsaho@users.noreply.github.com>
2023-04-09 17:25:26 -0700

    Added comments

--- assembly.s
+++ assembly.s
@@ -23,12 +23,12 @@ main:                     						# main function label
 .LFB6:                    						# label for start of function
 	pushq	%rbp             					# push rbp onto stack
 	movq	%rsp, %rbp       					# move the stack pointer to rbp
-
-# function 
 	subq	$32, %rsp        					# allocate 32 bytes of space on the stack for local variables
 	movq	%fs:40, %rax     					# read the address of the current thread's stack guard
 	movq	%rax, -8(%rbp)   					# store the stack guard value on the stack
 	xorl	%eax, %eax       					# set eax to zero
+	
+#Initialize variables
 	movl	$0, -28(%rbp)    					# initialize a variable to store the number of guesses
 	movl	$5, -24(%rbp)    					# initialize a variable to store the maximum value for the guess
 	movl	$1, -20(%rbp)    					# initialize a variable to store the minimum value for the guess
@@ -36,6 +36,8 @@ main:                     						# main function label
 	subl	-20(%rbp), %eax  					# subtract the minimum value from eax
 	addl	$1, %eax         					# add 1 to get the range of possible values
 	movl	%eax, -16(%rbp)  					# store the range of possible values
+
+#Generate a random number
 	movl	$0, %edi        					# set the first argument to zero for the time function call
 	call	time@PLT         					# call the time function to get the current time in seconds
 	movl	%eax, %edi       					# store the time value in edi for the srand function call
@@ -46,11 +48,14 @@ main:                     						# main function label
 	movl	-20(%rbp), %eax  					# load the minimum value into eax
 	addl %edx, %eax       						# Add the quotient of the division to %eax
 
+#Prompt for user guess
 movl %eax, -12(%rbp)  							# Move the result to the memory location at -12(%rbp)
 leaq .LC0(%rip), %rax 							# Load the address of the string .LC0 into %rax
 movq %rax, %rdi       							# Move the value in %rax to %rdi (for use in printf)
 movl $0, %eax         							# Move the value 0 to %eax (for use in printf)
 call printf@PLT      							# Call the printf function to print the string in %rdi
+
+#Scan user input
 leaq -32(%rbp), %rax  							# Load the address of the memory location at -32(%rbp) into %rax
 movq %rax, %rsi       							# Move the value in %rax to %rsi (for use in scanf)
 leaq .LC1(%rip), %rax 							# Load the address of the string .LC1 into %rax
@@ -58,14 +63,19 @@ movq %rax, %rdi       							# Move the value in %rax to %rdi (for use in scanf)
 movl $0, %eax         							# Move the value 0 to %eax (for use in scanf)
 call __isoc99_scanf@PLT 						# Call the scanf function to read an integer into the memory location at -32(%rbp)
 movl -32(%rbp), %eax  							# Move the value at -32(%rbp) to %eax
+
+#Compare guess
 cmpl %eax, -12(%rbp)  							# Compare the value at -12(%rbp) with the value at -32(%rbp)
 jne .L2               							# If the values are not equal, jump to .L2
+
+#User wins
 leaq .LC2(%rip), %rax 							# Load the address of the string .LC2 into %rax
 movq %rax, %rdi       							# Move the value in %rax to %rdi (for use in printf)
 movl $0, %eax         							# Move the value 0 to %eax (for use in printf)
 call printf@PLT      							# Call the printf function to print the string in %rdi
 jmp .L3               							# Jump to .L3
 
+#User loses
 .L2:
 	leaq	.LC3(%rip), %rax   					# load address of the format string for printf into %rax
 	movq	%rax, %rdi        				 	# move the format string address to the first argument register %rdi
